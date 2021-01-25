7dba3d28a367dd5d4f0da8037bb3f876b5fca010
Artur Matos <67583930+matos85@users.noreply.github.com>
2021-01-25 12:31:18 +0300

    Update lesson2_2.py

--- lesson2_2.py
+++ lesson2_2.py
@@ -1,21 +1,26 @@
 '''
+https://app.diagrams.net/#G15Xt2S6YHrGTG3cB_mzBjEa676TkBC4-i
+
  Посчитать четные и нечетные цифры введенного натурального числа.
  Например, если введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
-
 '''
 
 print('Введите натуральное число')
-k=int(input('Ваше число: '))
-z=1
+k = int(input('Ваше число: '))
+z = 0.1
 y = 0
 x = 0
-for i in range(len(str(k))):
-    if i == len(str(k)):
-        z = 1
-    else:
-        z*=10
-        if ((k//z)%2==0):
+for i in range(1, len(str(k)) + 1):
+    if len(str(i)) == len(str(k)):
+        if (k == 2) or (k == 4) or (k == 6) or (k == 8) or (k == 0):
             y += 1
         else:
             x += 1
-print(f'Нечетных: {y}, Четных: {x}')
+    else:
+        z *= 10
+    if ((k // z) % 2 == 0):
+        y += 1
+    else:
+        x += 1
+
+print(f'Нечетных: {x}, Четных: {y}')
