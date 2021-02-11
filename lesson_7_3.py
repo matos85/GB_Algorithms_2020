506fd4896fb52c3cdf1207d6b411a5b65a13f27f
Artur Matos <67583930+matos85@users.noreply.github.com>
2021-02-11 21:09:29 +0300

    Update lesson_7_3.py

--- lesson_7_3.py
+++ lesson_7_3.py
@@ -18,7 +18,7 @@ from collections import defaultdict
 my_dict = defaultdict(list)
 
 m = int(input('Введите  число для расчета длины массива: '))
-# m = 15
+
 
 SIZE_N = 2 * m + 1
 MIN_ITEM = -1_000_000
@@ -58,7 +58,7 @@ for i in mass:
 num = 0
 for k, v in my_dict.items():
     for j in range(len(v)):
-        if abs(v[j]) == m:
+#         if abs(v[j]) == m:
             a = k
         else:
             if v[j] / 2 > m:
@@ -71,3 +71,48 @@ for k, v in my_dict.items():
     #         a = k
 
 print(a)
+
+#Вариант 2: Честно скажу с ним мне подсказали
+# 
+# 
+# def heapify(nums, m_size, m_index):
+#     # Индекс наибольшего элемента считаем корневым индексом
+#     i_larg = m_index
+#     left_child = (2 * m_index) + 1
+#     right_child = (2 * m_index) + 2
+# 
+#     if left_child < m_size and nums[left_child] > nums[i_larg]:
+#         i_larg = left_child
+# 
+#     # То же самое для правого потомка корня
+#     if right_child < m_size and nums[right_child] > nums[i_larg]:
+#         i_larg = right_child
+# 
+#     # Если наибольший элемент больше не корневой, они меняются местами
+#     if i_larg != m_index:
+#         nums[m_index], nums[i_larg] = nums[i_larg], nums[m_index]
+#         heapify(nums, m_size, i_larg)
+# 
+# 
+# def heap_sort(nums):
+#     n = len(nums)
+#     for i in range(n, -1, -1):
+#         heapify(nums, n, i)
+# 
+#     for i in range(n - 1, 0, -1):
+#         nums[i], nums[0] = nums[0], nums[i]
+#         heapify(nums, i, 0)
+# 
+# 
+# heap_sort(mass)
+# 
+# n = len(mass)
+# if n % 2 == 0:
+#     median1 = mass[n // 2]
+#     median2 = mass[n // 2 - 1]
+#     median = (median1 + median2) / 2
+# else:
+#     median = mass[n // 2]
+# 
+# print("Медиана: " + str(median))
+
